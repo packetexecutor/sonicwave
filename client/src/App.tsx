294d3121f0f9d93d75805e8c8f5cb02dc5679495
packetexecutor <nguyenvietanh20143@gmail.com>
2025-04-18 13:29:23 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
-import { Switch, Route, useLocation } from "wouter";
-import { useHashLocation } from "./lib/hashLocation";
+mport { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
--1
-+4
+import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
-function Router() {
+// Hàm tiện ích để kiểm tra vị trí hiện tại của URL hash
+const useHashLocation = (): [string, (to: string) => void] => {
+  const getHash = () => window.location.hash.replace(/^#/, "") || "/";
+  const [loc, setLoc] = React.useState(getHash());
   
-  const [location, setLocation] = useHashLocation();
-  
-  return (
-    <Switch location={location} matcher={(a, b) => a === b}>
-      {/* Add pages below */}
-      <Route path="/" component={Home} />
-      {/* Fallback to 404 */}
+  React.useEffect(() => {

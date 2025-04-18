55e8cb0f7e4210239c9799928b3554d227ee0495
packetexecutor <nguyenvietanh20143@gmail.com>
2025-04-18 13:28:30 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { setupHashRouting } from "./lib/setupHashRouting";
 import "./index.css";
-
+setupHashRouting();
 createRoot(document.getElementById("root")!).render(<App />);

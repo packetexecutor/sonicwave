a23c4c10e568266e661f98234cbf1196517fa781
packetexecutor <nguyenvietanh20143@gmail.com>
2025-04-18 13:24:41 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,17 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
+import { useHashLocation } from "./lib/hashLocation";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+-1
++4
 import Home from "@/pages/Home";
-
 function Router() {
+  // Sử dụng location hook tùy chỉnh cho GitHub Pages
+  const [location, setLocation] = useHashLocation();
+  
   return (
-    <Switch>
+    <Switch location={location} matcher={(a, b) => a === b}>
       {/* Add pages below */}
       <Route path="/" component={Home} />
       {/* Fallback to 404 */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
